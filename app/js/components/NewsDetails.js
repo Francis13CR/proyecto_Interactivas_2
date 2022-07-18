c2fce62b9b1cbd472fa6e5c31501158bcbc5c347
francellaCC <102675185+francellaCC@users.noreply.github.com>
2022-07-18 12:14:08 -0600

    created component RelatedNews

--- app/js/components/NewsDetails.js
+++ app/js/components/NewsDetails.js
@@ -60,49 +60,16 @@ app.component('news-details', {
                   <div class="row g-0">
                         
                         <div class="justify-content-center">
-                            <button class="btn btn-primary mt-4 p-1 btn-sm " @click="close(1)"><i class="material-icons ">arrow_back</i></button>
-                            <h5 class="card-title text-center tp-serif fs-2 ">{{title}}</h5>
-                            <img :src="image" class="mt-2 img-fluid img-details mx-auto d-block mx-5 shadow" alt="...">
-                            <p class="card-text text-center"><small class="text-muted">{{date}}</small></p>
-                            <p class="card-text text-center">
-                                <button type="button" class="btn btn-outline-primary btn-sm" @click="clickLike(id)">
-                                        <i :id="id" class="fa-regular fa-thumbs-up" aria-hidden="true"></i><span class="">{{likes}}</span>
-                                </button>
-                            </p>
-                            <p class="card-text text-center mb-5">{{description}}</p>
+                            <button class="btn btn-primary mt-5 p-1 btn-sm " @click="close(1)"><i class="material-icons ">arrow_back</i></button>
+                            <h5 class="card-title mx-5 tp-serif fs-2 ">{{title}}</h5>
+                            <img :src="image" class="mt-2 img-fluid  img-details  d-block mx-5 shadow" alt="...">
+                            <p class="card-text  mx-5"><small class="text-muted">{{date}}</small></p>
+                            <p class="card-text  mx-5 mb-5">{{description}}</p>
                         </div>
                
                     </div>
                 </div>
-            </div>
-
-            <h5 class="card-text  border-dark border-top text-center fs-2">Noticias similares</h5>
-            <div class="row">
-                <div  v-for="(n,i) in all_news" class="col-md-4">
-                    <div class="card mb-5 card-margin h-75">
-                        <img class="card-img-top  img-fluid img-fix" :src="n.image" :alt = "n.title" >
-                        <div class=" card-img-overlay  overlay text-white text-center ">
-                            <div class="similar-content "  >
-                                <h5 class="card-title pb-1 ">{{n.title}}</h5>
-                                <h6 class="card-subtitle mb-1 ">{{n.subtitle}}</h6>
-                                <p class="card-text"><small >{{n.date}}</small></p>
-                                <p class="icon_like"><i class="fa-regular fa-thumbs-up " aria-hidden="true"></i>{{n.likes}}</p>
-                            </div>
-                            <p class="card-likes">
-                                <button type="button" class="btn btn-outline-info btn-sm " @click="clickNewsDetails(n.id)">
-                                <i class="fa-solid fa-eye text-white" aria-hidden="true"></i><span class="text-white">Detalles</span>
-                                </button>
-                            </p>
-                            <p class="mt-1 card-likes ">
-                                <button type="button" class="btn btn-outline-primary btn-sm" @click="clickLike(n.id)">
-                                    <i :id="n.id" class="fa-regular fa-thumbs-up text-white" aria-hidden="true"></i><span class="text-white">{{n.likes}}</span>
-                                </button>
-                            </p>  
-                        </div>
-                    </div>
-                </div>
-            </div>
-    
+            </div>    
         </div>
     `
 });
